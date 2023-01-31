b031d6ff3b3cfee48f75d67cfb1b00768e8c99b2
Keval Jain <kevalja@cybage.com>
2023-01-31 14:11:49 +0530

    handled json error

--- src/controllers/resetPassword.controller.js
+++ src/controllers/resetPassword.controller.js
@@ -1,35 +1,40 @@
-const userModel = require('../models/users.model')
-const { sendResetEmail } = require('../services/resetPassword.email')
-const { encryptPassword } = require('../services/password.encrypt')
+const userModel = require("../models/users.model");
+const { sendResetEmail } = require("../services/resetPassword.email");
+const { encryptPassword } = require("../services/password.encrypt");
 
 const forgetPassword = async (req, res) => {
-    try {
-        const email = req.body.email
+  try {
+    const email = req.body.email;
 
-        const [userDetails] = await userModel.getUserbyId(email)
+    const [userDetails] = await userModel.getUserbyId(email);
 
-        const userId = userDetails.iD
+    const userId = userDetails.iD;
 
-        const fullName = userDetails.fullName
+    const fullName = userDetails.fullName;
 
-        sendResetEmail(email, userId, fullName);
+    sendResetEmail(email, userId, fullName);
 
-        res.send("Reset mail sent to your email id.")
-    }
-    catch (err) {
-        res.send("Cannot send email. " + err)
-    }
-}
+    res.send({
+        message: "Reset mail sent to your email id."
+    });
+  } catch (err) {
+    res.send({ 
+        message: "Cannot send email. ",
+        error: err
+    });
+  }
+};
 
 const resetPassword = async (req, res) => {
+  const userId = parseInt(req.params.userId);
 
-    const userId = parseInt(req.params.userId)
+  const password = encryptPassword(req.body.password);
 
-    const password = encryptPassword(req.body.password)
+  const changepassword = await userModel.updatePassword(userId, password);
 
-    const changepassword = await userModel.updatePassword(userId, password)
+  res.send({
+    message: "Password Changed",
+  });
+};
 
-    res.send("password changed")
-}
-
-module.exports = { resetPassword, forgetPassword }
+module.exports = { resetPassword, forgetPassword };
